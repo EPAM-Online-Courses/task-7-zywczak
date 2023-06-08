9a3df4468db999ff55a5098e078e675ebcc2cf0a
zywczak <123404938+zywczak@users.noreply.github.com>
2023-06-08 11:28:02 +0200

    Update Villager.java

--- src/main/java/efs/task/reflection/model/Villager.java
+++ src/main/java/efs/task/reflection/model/Villager.java
@@ -1,5 +1,8 @@
 package efs.task.reflection.model;
 
+
+import efs.task.reflection.annotations.BuilderProperty;
+import efs.task.reflection.annotations.NotNull;
 /**
  * TODO Dodaj w odpowiednich miejscach adnotacje utworzone w pakiecie
  * <code>efs.task.reflection.annotations</code>
@@ -12,8 +15,10 @@ public class Villager implements Fighter {
   public static final String HIDDEN_VILLAGER_NAME = "Anonymous";
   public static final String HIDDEN_VILLAGER_DESC = "Hidden from the world, can hack anything that uses electricity";
 
+  @NotNull
   private final String name;
 
+  @NotNull
   private final String description;
   
   private Integer age;
@@ -33,12 +38,14 @@ public class Villager implements Fighter {
     this(name, HIDDEN_VILLAGER_DESC);
     this.age = age;
   }
-
-  public Villager(final String name, final String description) {
+  
+  @BuilderProperty(name = "init")
+  public Villager(@NotNull final String name, @NotNull final String description) {
     this.name = name;
     this.description = description;
   }
 
+  @BuilderProperty(name = "health")
   public void setHealth(int health) {
     this.health = health;
   }
